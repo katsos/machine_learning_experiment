f64ca8807dac9610359c4f711e9723ce9426fb57
Nikos Katsos <nikoskeyz@gmail.com>
2019-03-31 16:48:29 +0300

    split train and validation data

--- machine_learning.py
+++ machine_learning.py
@@ -38,3 +38,11 @@ print(dataset.groupby('class').size())
 # # scatter plot matrix
 # scatter_matrix(dataset)
 # plt.show()
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:, 0:4]
+Y = array[:, 4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
